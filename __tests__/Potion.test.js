7fc7d8e0da9f7353d936de66833339683e148174
Charlie Carter <charliec1665@gmail.com>
2022-09-12 17:13:04 -0400

    potion code refactor

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,8 +1,9 @@
 const Potion = require('../lib/Potion');
 
-test('creates a health potion object', () => {
-    const potion = new Potion('health');
+test('creates a random potion object', () => {
+    const potion = new Potion();
 
-    expect(potion.name).toBe('health');
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
