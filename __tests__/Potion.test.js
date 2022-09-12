2f2049da04e16d516df2376dbae26921c966141d
Charlie Carter <charliec1665@gmail.com>
2022-09-12 16:59:52 -0400

    wrote failing potion test

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,8 @@
+const Potion = require('../lib/Potion');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
